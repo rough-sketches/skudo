9f55054f5ecbdd3e081733c1e94b86e4895a99d5
rough-sketches <dawnitnow@gmail.com>
2026-02-14 03:09:58 +0530

    Bootstrap Cloud Run with dummy image and ignore-changes lifecycle

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,27 +14,27 @@ provider "google" {
 
 # Enable necessary APIs
 resource "google_project_service" "cloudrun" {
-  service = "run.googleapis.com"
+  service            = "run.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "cloudbuild" {
-  service = "cloudbuild.googleapis.com"
+  service            = "cloudbuild.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "artifactregistry" {
-  service = "artifactregistry.googleapis.com"
+  service            = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
 
 # Artifact Registry Repo
 resource "google_artifact_registry_repository" "repo" {
-  location      = var.region
-  repository_id = "skudo-repo"
-  description   = "Docker repository for Skudo App"
-  format        = "DOCKER"
-  depends_on    = [google_project_service.artifactregistry]
+  location               = var.region
+  repository_id          = "skudo-repo"
+  description            = "Docker repository for Skudo App"
+  format                 = "DOCKER"
+  depends_on             = [google_project_service.artifactregistry]
   cleanup_policy_dry_run = false
   cleanup_policies {
     id     = "keep-minimum-versions"
@@ -54,37 +54,43 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
         env {
-          name = "NEXT_PUBLIC_YOUTUBE_API_KEY"
+          name  = "NEXT_PUBLIC_YOUTUBE_API_KEY"
           value = var.youtube_api_key
         }
         env {
-          name = "NEXT_PUBLIC_FIREBASE_API_KEY"
+          name  = "NEXT_PUBLIC_FIREBASE_API_KEY"
           value = var.firebase_api_key
         }
         env {
-          name = "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"
+          name  = "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"
           value = var.firebase_auth_domain
         }
         env {
-          name = "NEXT_PUBLIC_FIREBASE_PROJECT_ID"
+          name  = "NEXT_PUBLIC_FIREBASE_PROJECT_ID"
           value = var.firebase_project_id
         }
         env {
-          name = "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"
+          name  = "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"
           value = var.firebase_storage_bucket
         }
-         env {
-          name = "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"
+        env {
+          name  = "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"
           value = var.firebase_messaging_sender_id
         }
         env {
-          name = "NEXT_PUBLIC_FIREBASE_APP_ID"
+          name  = "NEXT_PUBLIC_FIREBASE_APP_ID"
           value = var.firebase_app_id
         }
       }
     }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].spec[0].containers[0].image,
+    ]
   }
 
   traffic {
@@ -112,49 +118,6 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-# Cloud Build Trigger
-resource "google_cloudbuild_trigger" "push_trigger" {
-  name = "skudo-push-trigger"
-  
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = "^main$"
-    }
-  }
-
-  build {
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [
-        "build",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_API_KEY=${var.firebase_api_key}",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=${var.firebase_auth_domain}",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_PROJECT_ID=${var.firebase_project_id}",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=${var.firebase_storage_bucket}",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=${var.firebase_messaging_sender_id}",
-        "--build-arg=NEXT_PUBLIC_FIREBASE_APP_ID=${var.firebase_app_id}",
-        "--build-arg=NEXT_PUBLIC_YOUTUBE_API_KEY=${var.youtube_api_key}",
-        "-t", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest",
-        "."
-      ]
-    }
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["push", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"]
-    }
-    step {
-      name = "gcr.io/google.com/cloudsdktool/cloud-sdk"
-      entrypoint = "gcloud"
-      args = ["run", "deploy", "skudo-app", "--image", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest", "--region", var.region]
-    }
-    images = ["${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"]
-
-    options {
-      logging = "CLOUD_LOGGING_ONLY"
-    }
-  }
-
-  depends_on = [google_project_service.cloudbuild]
-}
+# NOTE: Cloud Build Trigger is managed via gcloud CLI (not Terraform)
+# because the GitHub connection requires OAuth which Terraform can't handle easily.
+# To create/update the trigger, run the setup-trigger.sh script.
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,3 +31,8 @@ variable "firebase_project_id" { type = string }
 variable "firebase_storage_bucket" { type = string }
 variable "firebase_messaging_sender_id" { type = string }
 variable "firebase_app_id" { type = string }
+
+variable "github_app_installation_id" {
+  description = "GitHub App installation ID for Cloud Build connection"
+  type        = number
+}
