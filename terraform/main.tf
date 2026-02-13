4adf804db29154a1aff47557ca2958a3213f36a4
rough-sketches <dawnitnow@gmail.com>
2026-02-14 02:28:50 +0530

    Add Terraform configuration and update gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,145 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.40.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Enable necessary APIs
+resource "google_project_service" "cloudrun" {
+  service = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudbuild" {
+  service = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Artifact Registry Repo
+resource "google_artifact_registry_repository" "repo" {
+  location      = var.region
+  repository_id = "skudo-repo"
+  description   = "Docker repository for Skudo App"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.artifactregistry]
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = ["skudo-app"]
+      keep_count            = 5
+    }
+  }
+}
+
+# Cloud Run Service
+resource "google_cloud_run_service" "default" {
+  name     = "skudo-app"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"
+        env {
+          name = "NEXT_PUBLIC_YOUTUBE_API_KEY"
+          value = var.youtube_api_key
+        }
+        env {
+          name = "NEXT_PUBLIC_FIREBASE_API_KEY"
+          value = var.firebase_api_key
+        }
+        env {
+          name = "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"
+          value = var.firebase_auth_domain
+        }
+        env {
+          name = "NEXT_PUBLIC_FIREBASE_PROJECT_ID"
+          value = var.firebase_project_id
+        }
+        env {
+          name = "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"
+          value = var.firebase_storage_bucket
+        }
+         env {
+          name = "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"
+          value = var.firebase_messaging_sender_id
+        }
+        env {
+          name = "NEXT_PUBLIC_FIREBASE_APP_ID"
+          value = var.firebase_app_id
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [google_project_service.cloudrun]
+}
+
+# Allow unauthenticated access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+# Cloud Build Trigger
+resource "google_cloudbuild_trigger" "push_trigger" {
+  name = "skudo-push-trigger"
+  
+  github {
+    owner = var.repo_owner
+    name  = var.repo_name
+    push {
+      branch = "^main$"
+    }
+  }
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest", "."]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"]
+    }
+    step {
+      name = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+      entrypoint = "gcloud"
+      args = ["run", "deploy", "skudo-app", "--image", "${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest", "--region", var.region]
+    }
+    images = ["${var.region}-docker.pkg.dev/${var.project_id}/skudo-repo/skudo-app:latest"]
+  }
+
+  depends_on = [google_project_service.cloudbuild]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+  description = "Google Cloud Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google Cloud Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "repo_owner" {
+  description = "GitHub Repository Owner"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "GitHub Repository Name"
+  type        = string
+}
+
+variable "youtube_api_key" {
+  description = "YouTube API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "firebase_api_key" { type = string }
+variable "firebase_auth_domain" { type = string }
+variable "firebase_project_id" { type = string }
+variable "firebase_storage_bucket" { type = string }
+variable "firebase_messaging_sender_id" { type = string }
+variable "firebase_app_id" { type = string }
